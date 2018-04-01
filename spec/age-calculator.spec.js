80de30458594d98432e65945909bcecc9b9d57a5
gold-mir <miranda.keith.42@gmail.com>
2018-03-31 19:37:31 -0700

    new spec and functionality

--- spec/age-calculator.spec.js
+++ spec/age-calculator.spec.js
@@ -11,4 +11,12 @@ describe('AgeCalculator', function(){
 
     expect(myAgeInSeconds).toEqual(662695992);
   });
+
+  it('should get the difference between two dates in seconds', function(){
+    let longTimeAgo = new Date('August 11, 1996');
+    let now = new Date(Date.now());
+
+    let timeDifference = ageCalc.getTimeDifference(now, longTimeAgo);
+    expect(timeDifference).toEqual(Math.floor((now.getTime() - longTimeAgo.getTime())/1000));
+  });
 });
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,5 +1,13 @@
 export class AgeCalculator {
   getAgeInSeconds(age){
-    
+    const secondsInYear = 31556952;
+    return age * secondsInYear;
+  }
+
+  getTimeDifference(date1, date2){
+    let date1MS = date1.getTime();
+    let date2MS = date2.getTime();
+
+    return Math.floor((date1MS - date2MS)/1000);
   }
 }
